740e51755c8836cd8ec29885c06a2f4b50c0dfc7
Rashmi Ranjan Panda <rrashmipanda2400@gmail.com>
2026-01-02 15:21:24 +0530

    probality in percentage predict model updated

--- model/predict.py
+++ model/predict.py
@@ -8,12 +8,10 @@ with open('model/model.pkl', 'rb') as f:
 #Actually it comes fromMLFLOW
 MODEL_VERSION = "1.0.0"
 
+
 # def predict_output(user_input:dict):
-
 #     input_df=pd.DataFrame([user_input])
-
 #     output= model.predict(input_df)[0]
-
 #     return output
 
 
